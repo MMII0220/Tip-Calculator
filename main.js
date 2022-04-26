5eb7f69cd7c9450436e74573fd7d6621db434314
MMII0220 <abdulloevmuhammadcon36@gmail.com>
2022-04-26 17:22:27 +0300

    finished html and css

---
+++ main.js
@@ -0,0 +1,10 @@
+const dol = document.querySelector('.dollar'),
+  peo = document.querySelector('.people'),
+  perc = document.querySelectorAll('.percentage'),
+  calc = document.querySelector('.calc');
+
+/* console.log(perc[5]); */
+
+perc.forEach((item) => {
+  console.log(item.value);
+});
