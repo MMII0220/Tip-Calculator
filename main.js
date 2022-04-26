165ddb661089683cab6d96c0f3ae6a41006828ae
MMII0220 <abdulloevmuhammadcon36@gmail.com>
2022-04-26 20:15:23 +0300

    smth

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
-const dol = document.querySelector('.dollar'),
-  peo = document.querySelector('.people'),
-  perc = document.querySelectorAll('.percentage'),
-  calc = document.querySelector('.calc');
+const money = document.querySelector('.dollar'),
+  peop = document.querySelector('.people'),
+  perc = document.querySelector('.percentage'),
+  calc = document.querySelector('.calc'),
+  total = document.querySelector('.tip-amount');
 
-/* console.log(perc[5]); */
+let expression;
 
-perc.forEach((item) => {
-  console.log(item.value);
+for (let i = 1; i < perc.length; i++) {
+  if (perc[i].value === '0.3') {
+    expression = Number(money.value * perc[i].value * 100) / Number(peop.value);
+  }
+}
+
+calc.addEventListener('click', () => {
+  total.style.cssText = 'visibility: auto;';
+  total.innerHTML = `Tip ${expression} each`;
 });
