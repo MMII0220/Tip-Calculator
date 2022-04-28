5b6e5e10c76944e651b36d2def0ba07ea3c7852a
MMII0220 <abdulloevmuhammadcon36@gmail.com>
2022-04-28 04:48:17 +0300

    finishes calculation

--- main.js
+++ main.js
@@ -1,18 +1,40 @@
 const money = document.querySelector('.dollar'),
   peop = document.querySelector('.people'),
-  perc = document.querySelector('.percentage'),
+  perc = document.querySelectorAll('.percentage'),
   calc = document.querySelector('.calc'),
   total = document.querySelector('.tip-amount');
 
+perc.forEach((num) => {
+  console.log(num);
+});
+
 let expression;
 
-for (let i = 1; i < perc.length; i++) {
-  if (perc[i].value === '0.3') {
-    expression = Number(money.value * perc[i].value * 100) / Number(peop.value);
-  }
-}
-
 calc.addEventListener('click', () => {
-  total.style.cssText = 'visibility: auto;';
+  perc.forEach((item) => {
+    switch (item.value) {
+      case '0.3':
+        expression = Math.ceil((money.value * 0.3) / peop.value);
+        break;
+      case '0.2':
+        expression = Math.ceil((money.value * 0.2) / peop.value);
+        break;
+      case '0.15':
+        expression = Math.ceil((money.value * 0.15) / peop.value);
+        break;
+      case '0.1':
+        expression = Math.ceil((money.value * 0.1) / peop.value);
+        break;
+      case '0.05':
+        expression = Math.ceil((money.value * 0.05) / peop.value);
+        break;
+      default:
+        expression = 'fill input';
+    }
+  });
+
   total.innerHTML = `Tip ${expression} each`;
+  total.classList.add('show');
+
+  setTimeout(total.classList.add('show'), 4000);
 });
